fe80de04286067fd970073c5c8deedaa9b843903
hamza.bejtic <hamzabejtic97@gmail.com>
2022-09-10 17:59:24 +0200

    CORSMiddleware added in main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from user import register, auth, editprofile
 from posts import post, comment
 
 app = FastAPI()
+
+# CORS:
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes:
 
 app.include_router(register.router)
 app.include_router(auth.router)
